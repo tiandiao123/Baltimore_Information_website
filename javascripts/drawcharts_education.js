f80785d0a25ea3b7fd5ceccbae78f7e5e83591ca
tiandiao123 <lixx3527@gmail.com>
2017-01-22 11:15:47 -0500

    more work has been done

--- javascripts/drawcharts_education.js
+++ javascripts/drawcharts_education.js
@@ -5,11 +5,11 @@
     bindto:'#chart_education',
     data: {
         columns: [
-            ['white', 8552, 8661],
-            ['black', 4899, 4984],
-            ['asian',1527,1531],
-            ['Hispanic',755,797],
-            ['multi',367,445],
+            ['white people who received degrees', 8552, 8661],
+            ['black people who received degrees', 4899, 4984],
+            ['asian who received degrees',1527,1531],
+            ['Hispanic who received degrees',755,797],
+            ['multi who received degrees',367,445],
             ['unknown',673,569]
         ],
         type: 'bar'
@@ -24,6 +24,43 @@
           x: {
               type: 'category',
               categories: time,
+              tick: {
+                multiline: false
+              }
+          }
+      },
+      zoom: {
+          enabled: true
+      },
+      legend: {
+          position: 'right'
+      } 
+
+});
+
+
+
+schools=['johns hopkins','university of Maryland-Baltimore','Loyola University Maryland','All state Career Baltimore','University of Baltmore'];
+
+  var chart2 = c3.generate({
+    bindto:'#chart_education2',
+    data: {
+        columns: [
+            ['the number of male students', 3701, 534,600,892,654],
+            ['the number of female students', 4057, 1517,1028,463,838]
+        ],
+        type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        },
+      },
+
+     axis: {
+          x: {
+              type: 'category',
+              categories: schools,
               tick: {
                 multiline: false
               }
